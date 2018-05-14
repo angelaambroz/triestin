f757b4a0fe0cf73368bc947d2052ff86da82954a
Angela Ambroz <angela.ambroz@gmail.com>
2018-05-14 17:22:45 -0400

    Tokenizer, complete

--- trst.py
+++ trst.py
@@ -3,6 +3,14 @@
 import re
 
 # Tokenizer/Lexer
+class Token():
+    def __init__(self, tipo, value):
+        self.tipo = tipo
+        self.value = value
+
+    def __str__(self):
+        return f"Token(type='{self.tipo}', value='{self.value}')"
+
 class Tokenizer():
     def __init__(self, filename):
         self.file = filename
@@ -23,20 +31,26 @@ class Tokenizer():
             self.code = [x.replace('\n', '') for x in f.readlines()]
         
     def tokenize(self):
-        for (tipo, rgx) in self.TOKEN_TYPES:
-            for chunk in self.code:
-                z = re.findall(rgx_, chunk)
-                if z:
-                    print(z)
+        tokens = []
+        for chunk in self.code:
+            tokens.append(self.tokenize_one_token(chunk))
 
-
-
+        return tokens
+                    
+    def tokenize_one_token(self, chunk):
+        for (tipo, rgx_) in self.TOKEN_TYPES:
+            z = re.findall(rgx_, chunk)
+            if z:
+                return Token(tipo, z[0])
 
 tokens = Tokenizer("trst.src").tokenize()
-
-
+for i in tokens:
+    print(i)
 
 # Parser
+
+
+
 
 # Code generator
 
